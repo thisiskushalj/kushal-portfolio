719bd84d32b616d0c45d1feb99d107201c95ca5a
Kushal J <kushalj400231@gmail.com>
2025-07-02 17:24:03 +0530

    update path in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { ThemeProvider } from './Theme/ThemeContext.jsx'
+import { ThemeProvider } from './Mode/ThemeContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
