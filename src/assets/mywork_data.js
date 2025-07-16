5096237f69c6c269a0e942ca48cb249a7297af26
Kushal J <kushalj400231@gmail.com>
2025-07-16 21:08:06 +0530

    Update new project in project section

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,41 +1,50 @@
 import project1_img from '../assets/project_1.png'
-import project2_img from '../assets/project_2.jpeg'
+import project2_img from '../assets/project_2.png'
 import project3_img from '../assets/project_3.png'
 import project4_img from '../assets/project_4.png'
 import project5_img from '../assets/project_5.png'
-import project6_img from '../assets/project_6.png'
+import project6_img from '../assets/project_6.jpeg'
+import project7_img from '../assets/project_7.png'
+
+
 
 const mywork_data = [
     {
         w_no:1,
-        w_name:"Spotify Clone",
+        w_name:"Fresh Basket",
         w_img:project1_img,
-        w_link: "https://spotify-clone-six-bay.vercel.app/"
+        w_link: "https://fresh-basket-mu.vercel.app/"
     },
     {
         w_no:2,
-        w_name:"Parallax website",
-        w_img:project2_img
-    },
+        w_name:"Spotify Clone",
+        w_img:project2_img,
+        w_link: "https://spotify-clone-six-bay.vercel.app/"
+    },   
     {
         w_no:3,
-        w_name:"Crypto Website",
+        w_name:"QR Code Generator",
         w_img:project3_img
     },
     {
         w_no:4,
-        w_name:"QR Code Generator",
+        w_name:"Resort Homepage",
         w_img:project4_img
     },
     {
         w_no:5,
-        w_name:"Resort Homepage",
+        w_name:"Weatherly",
         w_img:project5_img
     },
     {
         w_no:6,
-        w_name:"Weatherly",
+        w_name:"Parallax website",
         w_img:project6_img
+    },
+     {
+        w_no:7,
+        w_name:"Crypto Website",
+        w_img:project7_img
     },
 ]
  
